59870d9d4c47f309721b893f3b6749e42337cffb
kfunkin <32571927+kfunkin@users.noreply.github.com>
2019-06-19 22:06:23 +0100

    added drag to logo

--- main.js
+++ main.js
@@ -131,3 +131,63 @@ function seekSong() {
 /*function adjustVolume() {
   song.volume = volumeSlider.value;
 }*/
+
+//DRAG FUNCTION!!************************************************************************************************************
+var logo = document.querySelector("#logo");
+var dragContainer = document.querySelector(".drag-container");
+
+var active = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
+
+dragContainer.addEventListener("touchstart", dragStart, false);
+dragContainer.addEventListener("touchend", dragEnd, false);
+dragContainer.addEventListener("touchmove", drag, false);
+
+function dragStart(e) {
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
+  if (e.target === logo) {
+    active = true;
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialX = currentY;
+
+  active = false;
+}
+
+function drag(e) {
+  if (active) {
+
+    e.preventDefault();
+
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+    }
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, logo);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " +yPos +"px, 0)";
+}
